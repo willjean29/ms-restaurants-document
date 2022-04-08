4e323e4460afb667e277e47ac95fbdec36cab84b
Jean Osco <willjean29@gmail.com>
2022-04-08 12:30:31 -0500

    change port

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const app = require("./express-app");
 const dotenv = require("dotenv");
 dotenv.config();
 async function main() {
-  await app.listen(process.env.PORT);
-  console.log("listening to port ", process.env.PORT);
+  await app.listen(5000);
+  console.log("listening to port ", 5000);
 }
 
 main();
